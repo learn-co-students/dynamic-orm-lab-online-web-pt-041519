832a97b0195992155f4877b58482441664462d9d
Erin Ponsonby <erinponsonby@gmail.com>
2019-06-24 13:28:30 -0400

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -49,27 +49,22 @@ class InteractiveRecord
     end
 
     def self.find_by_name(name)
-        sql = "SELECT * FROM #{self.table_name} WHERE name = '?'"
-        DB[:conn].execute(sql, name)
+        sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+        DB[:conn].execute(sql)
     end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
+    def self.find_by(hash)
+        hash.collect do |key, value|
+            sql = "SELECT * FROM #{self.table_name} WHERE #{key} = '#{value}'"
+            @return_value = DB[:conn].execute(sql)
+        end
+            @return_value.flatten
+    end
 end
+
+
+
+
+
+
+
