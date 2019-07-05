01d412651f4a3ac5a75b42af0507cde7dfdf223f
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-07-05 14:45:53 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,9 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
+require_relative "../config/environment.rb"
+require 'active_support/inflector'
+
 class InteractiveRecord
 
   def self.table_name
@@ -8,50 +11,60 @@ class InteractiveRecord
   end
 
   def self.column_names
-    DB[:conn].results_as_hash = true
-
-    sql = "pragma table_info('#{table_name}')"
-
-    table_info = DB[:conn].execute(sql)
     column_names = []
-    table_info.each do |row|
-      column_names << row["name"]
+    sql = "PRAGMA table_info('#{table_name}')" #give you the hash of info
+    table_info = DB[:conn].execute(sql)
+
+    table_info.each do |col| #iterate over the array of hashes
+      column_names << col["name"] #this gives you the value for the key "name"
     end
-    column_names.compact
+    column_names.compact #to get rid of any nulls
   end
 
-  def initialize(options={})
-    options.each do |property, value|
-      self.send("#{property}=", value)
-    end
-  end
 
-  def save
-    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
-    DB[:conn].execute(sql)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
-    self
+  def initialize(options = {}) #pass in a hash
+    options.each do |key,value|
+      self.send(("#{key}="),value)
+    end
   end
 
   def table_name_for_insert
     self.class.table_name
   end
 
-  def values_for_insert
-    values = []
-    self.class.column_names.each do |col_name|
-      values << "'#{send(col_name)}'" unless send(col_name).nil?
-    end
-    values.join(", ")
-  end
-
   def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+    self.class.column_names.delete_if {|column_name| column_name == "id"}.join (", ")
   end
 
-def self.find_by_name(name)
-  sql = "SELECT * FROM #{self.table_name} WHERE name = '?'"
-  DB[:conn].execute(sql, name)
-end
+  def values_for_insert
+    values_array = []
+    self.class.column_names.each do |column_name|
+      #get the value for each attribute name
+      values_array << "'#{send(column_name)}'" unless send(column_name).nil?
+    end
+    values_array.join(", ")
+  end
+
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
+    row = DB[:conn].execute(sql,name)
+  end
+
+  def self.find_by(attribute)
+    attribute_key = attribute.keys.join()
+    attrubute_value = attribute.values.first
+    sql =<<-SQL
+      SELECT * FROM #{self.table_name}
+      WHERE #{attribute_key} = "#{attrubute_value}"
+      LIMIT 1
+    SQL
+    row = DB[:conn].execute(sql)
+  end
 
 end
